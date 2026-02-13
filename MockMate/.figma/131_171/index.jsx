bcfa9239535b4fdbe5d5a0c070eaac1cb3d2c1ab
Lechcher <lechcher@mail.com>
2026-02-13 14:04:09 +0700

    Backup

---
+++ MockMate/.figma/131_171/index.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.interviewDetail}>
+      <div className={styles.rectangle5}>
+        <div className={styles.aIPhoneXBarsStatusDe}>
+          <p className={styles.aTime}>9:41</p>
+          <img src="../image/mlc5bprf-3b6amiu.svg" className={styles.container} />
+        </div>
+        <div className={styles.autoWrapper}>
+          <div className={styles.rectangle9}>
+            <div className={styles.rectangle10} />
+            <img src="../image/mlc5bprg-6tv60bd.svg" className={styles.frame} />
+            <div className={styles.frame170}>
+              <div className={styles.frame169}>
+                <div className={styles.frame168}>
+                  <div className={styles.frame167}>
+                    <div className={styles.frame4}>
+                      <img
+                        src="../image/mlc5bprg-vo1ch1x.svg"
+                        className={styles.frame2}
+                      />
+                    </div>
+                    <div className={styles.frame163}>
+                      <p className={styles.seniorBackendEnginee}>
+                        Senior Backend Engineer
+                      </p>
+                      <p className={styles.estimatedTime15Mins}>
+                        Estimated time: 15 mins
+                      </p>
+                    </div>
+                  </div>
+                  <div className={styles.frame162}>
+                    <div className={styles.frame159}>
+                      <img
+                        src="../image/mlc5bprg-mvn1fo2.svg"
+                        className={styles.frame3}
+                      />
+                      <p className={styles.iT}>IT</p>
+                    </div>
+                    <div className={styles.frame160}>
+                      <img
+                        src="../image/mlc5bprg-cq96xk9.svg"
+                        className={styles.frame3}
+                      />
+                      <p className={styles.hard}>Hard</p>
+                    </div>
+                    <div className={styles.frame161}>
+                      <img
+                        src="../image/mlc5bprg-azr1rfg.svg"
+                        className={styles.frame3}
+                      />
+                      <p className={styles.a10Questions}>10 Questions</p>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.frame172}>
+                  <div className={styles.frame164}>
+                    <img
+                      src="../image/mlc5bprg-oczka8i.svg"
+                      className={styles.frame3}
+                    />
+                    <p className={styles.reviews}>Reviews</p>
+                  </div>
+                  <div className={styles.frame171}>
+                    <p className={styles.a4984Reviews3}>
+                      <span className={styles.a4984Reviews}>4.9&nbsp;</span>
+                      <span className={styles.a4984Reviews2}>(84 reviews)</span>
+                    </p>
+                    <img
+                      src="../image/mlc5bprg-vzo9ed4.svg"
+                      className={styles.frame5}
+                    />
+                  </div>
+                </div>
+                <div className={styles.frame165}>
+                  <div className={styles.frame1642}>
+                    <div className={styles.frame6}>
+                      <div className={styles.vector3}>
+                        <div className={styles.vector2}>
+                          <div className={styles.vector} />
+                        </div>
+                      </div>
+                    </div>
+                    <p className={styles.reviews}>Focus Area</p>
+                  </div>
+                  <p className={styles.theAiWillTestYourKno}>
+                    The AI will test your knowledge on Architecture, Database
+                    Optimization, Scalability, and Clean Code. Be prepared to
+                    explain your choices in distributed systems design.
+                  </p>
+                </div>
+              </div>
+              <div className={styles.frame166}>
+                <div className={styles.frame62}>
+                  <p className={styles.startNow}>Start Now</p>
+                  <img
+                    src="../image/mlc5bprg-7tpukar.svg"
+                    className={styles.frame7}
+                  />
+                </div>
+                <p className={styles.readyToTestYourSkill}>
+                  Ready to test your skills?&nbsp;
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.aIPhoneXHomeIndicato}>
+            <div className={styles.line} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
